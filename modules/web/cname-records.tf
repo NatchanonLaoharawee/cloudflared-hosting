70fb1d37bc0eea5724b79162d29753dc855fd5c8
Natchanon Laoharawee <s4006840@student.rmit.edu.au>
2025-11-18 13:55:33 +1100

    fix(cname): added cname records for web

--- modules/web/cname-records.tf
+++ modules/web/cname-records.tf
@@ -31,9 +31,63 @@ resource "cloudflare_record" "mail" {
 }
 
 # Piot tunnel (proxied)
-resource "cloudflare_record" "piot" {
+resource "cloudflare_record" "agent_piot" {
   zone_id = var.zone_id
-  name    = "agent.piot"
+  name    = "agent-piot"
+  content = "e8aa11a4-c266-4799-86a2-6ea01103e91e.cfargotunnel.com"
+  type    = "CNAME"
+  ttl     = 1
+  proxied = true
+}
+
+resource "cloudflare_record" "master_piot" {
+  zone_id = var.zone_id
+  name    = "master-piot"
+  content = "e8aa11a4-c266-4799-86a2-6ea01103e91e.cfargotunnel.com"
+  type    = "CNAME"
+  ttl     = 1
+  proxied = true
+}
+
+resource "cloudflare_record" "room1_piot" {
+  zone_id = var.zone_id
+  name    = "room1-piot"
+  content = "e8aa11a4-c266-4799-86a2-6ea01103e91e.cfargotunnel.com"
+  type    = "CNAME"
+  ttl     = 1
+  proxied = true
+}
+
+resource "cloudflare_record" "room2_piot" {
+  zone_id = var.zone_id
+  name    = "room2-piot"
+  content = "e8aa11a4-c266-4799-86a2-6ea01103e91e.cfargotunnel.com"
+  type    = "CNAME"
+  ttl     = 1
+  proxied = true
+}
+
+resource "cloudflare_record" "room3_piot" {
+  zone_id = var.zone_id
+  name    = "room3-piot"
+  content = "e8aa11a4-c266-4799-86a2-6ea01103e91e.cfargotunnel.com"
+  type    = "CNAME"
+  ttl     = 1
+  proxied = true
+}
+
+resource "cloudflare_record" "room4_piot" {
+  zone_id = var.zone_id
+  name    = "room4-piot"
+  content = "e8aa11a4-c266-4799-86a2-6ea01103e91e.cfargotunnel.com"
+  type    = "CNAME"
+  ttl     = 1
+  proxied = true
+}
+
+resource "cloudflare_record" "room5_piot" {
+  zone_id = var.zone_id
+  name    = "room5-piot"
   content = "e8aa11a4-c266-4799-86a2-6ea01103e91e.cfargotunnel.com"
   type    = "CNAME"
   ttl     = 1
